a20bf7379de4b0566c1d421febbbe8d5079c67a3
LaChe <27337779+LarisaChe@users.noreply.github.com>
2023-09-19 23:04:16 +0300

    netology_jdata36_7 spring_method_security/task1 Безопасные методы

--- netology_jdata36_6/src/main/java/ru/netology/controller/PersonController.java
+++ netology_jdata36_6/src/main/java/ru/netology/controller/PersonController.java
@@ -1,5 +1,9 @@
 package ru.netology.controller;
 
+import jakarta.annotation.security.RolesAllowed;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,30 +22,41 @@ public class PersonController {
     }
 
     @GetMapping("/persons/by-city")
+    @PreAuthorize("hasAnyRole('WRITE', 'DELETE')")
         public List<Person> getPersonsFromOneCity(@RequestParam("city") String city) {
         return service.getPersonsByCity(city);
     }
 
     @GetMapping("/persons/younger-than-age") //younger-than-age
+    @RolesAllowed({"READ"})
     public List<Person> getPersonsYoungerAge(@RequestParam("age") int age) {
         return service.getPersonsYoungerAge(age);
     }
 
     @GetMapping("/persons/by-name-surname")
+    @Secured("ROLE_DELETE")
     public Optional<Person> getPersonsByNameAndSurname(@RequestParam("name") String name, @RequestParam("surname") String surname) {
         return service.getPersonsByNameAndSurname(name, surname);
     }
 
     @GetMapping("/persons/by-id")
+    @RolesAllowed({"READ", "WRITE"})
     public Person getPersonById(@RequestParam("id") Long id) {
         return service.getPersonById(id);
     }
 
     @GetMapping("/persons/all-sort-by-surname")
+    @PostAuthorize("hasRole('READ')")
     public List<Person> getPersonsSortBySurname(){
         return service.getPersonsSortBySurname();
     }
 
     @GetMapping("/all-cities")
     public List<String> getAllCities() { return service.getAllCities(); }
+
+    @PreAuthorize("isAuthenticated().#username == authentication.principal.username")
+    @GetMapping("/hi")
+    public String getHi1(@RequestParam String username) {
+        return  "Hello, " + username; }
+
 }
--- netology_jdata36_6/src/main/java/ru/netology/security/SecurityConfiguration.java
+++ netology_jdata36_6/src/main/java/ru/netology/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package ru.netology.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,14 +11,15 @@ import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)  // prePostEnabled = true, - не указываю, потому что дефолтная
 public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/", "/all-cities").permitAll()
-                        .requestMatchers("/persons/").hasRole("USER")
+                        //.requestMatchers("/", "/all-cities").permitAll()
+                        //.requestMatchers("/persons/").hasRole("WRITE")
                         .anyRequest().authenticated())
                 .formLogin(
                         (form) -> form.permitAll())
@@ -27,12 +29,32 @@ public class SecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("ppp")
-                .password("123")
-                .roles("USER")
+        UserDetails userR = User.withDefaultPasswordEncoder()
+                .username("Kim")
+                .password("111")
+                .roles("READ")
                 .build();
-        return new InMemoryUserDetailsManager(user);
+        UserDetails userW = User.withDefaultPasswordEncoder()
+                .username("Pak")
+                .password("222")
+                .roles("WRITE")
+                .build();
+        UserDetails userD = User.withDefaultPasswordEncoder()
+                .username("Ten")
+                .password("333")
+                .roles("DELETE")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("Li")
+                .password("444")
+                .roles("READ", "WRITE")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("adm")
+                .password("123")
+                .roles("READ", "WRITE", "DELETE")
+                .build();
+        return new InMemoryUserDetailsManager(userR, userW, userD, user, admin);
     }
 
 }
